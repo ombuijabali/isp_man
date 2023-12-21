7b7d72e72b984ce2dc1eca70a8c1311fabd429a7
ombuijabali <127888271+ombuijabali@users.noreply.github.com>
2023-12-21 13:27:03 +0300

    Update settings.py

--- isp/settings.py
+++ isp/settings.py
@@ -97,18 +97,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "isp.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "d4q2a8k44hnn79",
-        "USER": "sjeazbvwvwfhxg",
-        "PASSWORD": "7dfab32f30ff952772d2b0910996979995ceb63d858d013739cf9b5ebb3f1a5e",
-        "HOST": "ec2-44-206-204-65.compute-1.amazonaws.com",
-        "PORT": "5432",
-    }
-}
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
