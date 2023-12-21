752379c74ebd17f9593e267d9d2061f29719a224
ombuijabali <127888271+ombuijabali@users.noreply.github.com>
2023-12-21 09:25:00 +0300

    Update settings.py

--- isp/settings.py
+++ isp/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'gdal', 'gdal304.dll')
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH', '')
 
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (-1.282385, 36.968488),
