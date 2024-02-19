d7d8dc261cd39fe35c91d07a43f00377b11b2ccd
Simion Ombui <127888271+ombuijabali@users.noreply.github.com>
2024-02-19 20:38:37 +0300

    Update settings.py

--- isp/settings.py
+++ isp/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "main.middleware.coop_middleware.CrossOriginOpenerPolicyMiddleware",
 ]
 
 ROOT_URLCONF = "isp.urls"
